c7259b943387b37a1a3e9187b9598f78fb1d807b
KotelnikovNS <91054969+KotelnikovNS@users.noreply.github.com>
2021-11-16 23:34:17 +0300

    Update UserInterfaceView.java

--- Lesson7_HW/UserInterfaceView.java
+++ Lesson7_HW/UserInterfaceView.java
@@ -14,14 +14,14 @@ public class UserInterfaceView {
             String city = scanner.nextLine();
 
             System.out.println("Введите 1 для получения погоды на сегодня;" +
-                    "Введите 5 для прогноза на 5 дней; Введите 88 для получения данных из базы;  Для выхода введите 0:");
+                    "Введите 5 для прогноза на 5 дней; Для выхода введите 0:");
 
             String command = scanner.nextLine();
 
             if (command.equals("0")) break;
-            if (command != "1" || command != "5" || command != "0" || command != "88") {
+            if (command != "1" || command != "5" || command != "0") {
                 System.out.println("Неа... Введите 1 для получения погоды на сегодня;" +
-                        "Введите 5 для прогноза на 5 дней; Введите 88 для получения данных из базы;  Для выхода введите 0:");
+                        "Введите 5 для прогноза на 5 дней; Для выхода введите 0:");
 
                 try {
                     controller.getWeather(command, city);
