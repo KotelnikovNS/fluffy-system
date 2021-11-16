a0999425047dd884f0f8ca5f25406d494eb8c522
KotelnikovNS <91054969+KotelnikovNS@users.noreply.github.com>
2021-11-16 23:28:38 +0300

    Update UserInterfaceView.java

--- Lesson7_HW/UserInterfaceView.java
+++ Lesson7_HW/UserInterfaceView.java
@@ -1,4 +1,4 @@
-package Lesson6_HW;
+package Lesson7_HW;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -14,18 +14,20 @@ public class UserInterfaceView {
             String city = scanner.nextLine();
 
             System.out.println("Введите 1 для получения погоды на сегодня;" +
-                    "Введите 5 для прогноза на 5 дней;  Для выхода введите 0:");
+                    "Введите 5 для прогноза на 5 дней; Введите 88 для получения данных из базы;  Для выхода введите 0:");
 
             String command = scanner.nextLine();
 
-            //TODO* Сделать метод валидации пользовательского ввода
-
             if (command.equals("0")) break;
+            if (command != "1" || command != "5" || command != "0" || command != "88") {
+                System.out.println("Неа... Введите 1 для получения погоды на сегодня;" +
+                        "Введите 5 для прогноза на 5 дней; Введите 88 для получения данных из базы;  Для выхода введите 0:");
 
-            try {
-                controller.getWeather(command, city);
-            } catch (IOException e) {
-                e.printStackTrace();
+                try {
+                    controller.getWeather(command, city);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
