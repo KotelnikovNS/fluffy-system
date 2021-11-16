77cf9d3a3a3edcaaed4e136ca01e0e6beed1a07c
KotelnikovNS <91054969+KotelnikovNS@users.noreply.github.com>
2021-11-16 23:33:02 +0300

    Update WeatherModel.java

--- Lesson7+_HW/WeatherModel.java
+++ Lesson7+_HW/WeatherModel.java
@@ -1,8 +1,10 @@
 package Lesson7_HW;
 
 import java.io.IOException;
+import Lesson7_HW.WeatherBase;
 
 public interface WeatherModel {
     void getWeather(String selectedCity, Period period) throws IOException;
 
+    public List<WeatherBase> getSavedToWeatherBase();
 }
